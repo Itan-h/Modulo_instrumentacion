19c36e3557c7e6a37a2df90e4482a9c24cf90d9d
Ita <itaherlu78@gmail.com>
2024-05-26 21:40:05 -0600

    Class and pins of sensors

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from machine import SPI,Pin
-import st7789
-import time
-import vga1_bold_16x32 as font
-import caudal
+from machine import Pin, time
+import sensores as s
 from rotary_irq import RotaryIRQ
 
-valve = caudal.valvula(4)
-sensor = caudal.caudalimetro()
-bomba = caudal.bomba(15)
-
+valve = s.Valvula(4)
+sensor = s.Caudalimetro(9)
+bomba = s.Bomba(15)
+ultrasonic = s.Ultrasonico(12, 13)
+hzt1_tanque1 = s.Sensor_switch(6)
+hzt2_tanque1 = s.Sensor_switch(7)
+hzt1_tanque2 = s.Sensor_switch(8)
+hzt2_tanque2 = s.Sensor_switch(9)
 btn = Pin(18, Pin.IN, Pin.PULL_UP)#boton del encoder
 encoder = RotaryIRQ(pin_num_clk=21,
               pin_num_dt=19,
@@ -17,17 +18,7 @@ encoder = RotaryIRQ(pin_num_clk=21,
               reverse=True,
               range_mode=RotaryIRQ.RANGE_WRAP)
 
-tft = st7789.ST7789(
-    SPI(2, baudrate=30000000, polarity=1, phase=1, sck=Pin(18), mosi=Pin(23)),
-    240,
-    320,
-    reset=Pin(5, Pin.OUT),
-    cs=Pin(16, Pin.OUT),
-    dc=Pin(17, Pin.OUT),
-    rotation=0
-   # buffer_size=240*200*2
-    )
-tft.init()
+
 
 while True:
     time.sleep(0.25)
--- sensores.py
+++ sensores.py
@@ -1,4 +1,4 @@
-from machine import Pin, SPI
+from machine import Pin, SPI, UART, PWM
 import machine, time, st7789
 
 # FUENTES DISPONIBLES
@@ -64,3 +64,75 @@ class Display:
             self.tft.fill_rect(x, y, width, height, st7789.RED)
         
         self.tft.text(font2, label, xl, yl, st7789.WHITE)
+
+class Caudalimetro:
+    def __init__(self, rx_pin=3, tx_pin=1):
+        self.uart = UART(1, baudrate=115200, tx=tx_pin, rx=rx_pin)
+        self.uart.init(115200, bits=8, parity=None, stop=1)
+        self.freq = 0
+        self.ltprh = 0
+        self.accumulated = 0
+        self.get_freq()
+
+    def get_freq(self):
+        self.msg=self.uart.readline()
+        if self.msg==None:
+            pass
+        else:
+            try:
+                self.freq=int(self.msg)
+            except:
+                pass
+        return self.freq
+
+    def get_lthr(self):
+        self.ltprh=(self.get_freq()*60)/7.5
+        return self.ltprh
+
+
+class Valvula:
+    def __init__(self, pin):
+        self.pin = Pin(pin, Pin.OUT)
+
+    def set_state(self, bool):
+        self.pin.value(bool)
+
+    def get_state(self):
+        return self.pin.value()
+
+class Bomba:
+    def __init__(self, pin):
+        self.pwm = PWM(Pin(pin), freq=1000, duty=0)
+        self.sp = 0
+        self.cv = 0
+        self.cv1 = 0
+        self.error = 0
+        self.error1 = 0
+        self.error2 = 0
+
+        self.Kp = 10
+        self.Ki = 20
+        self.Kd = 0.01
+        self.Tm = 0.25
+    
+    def on_pid(self, set_point, procces_v):
+        self.sp = set_point
+        self.error = self.sp - procces_v
+        
+        self.cv = self.cv1 + (self.Kp + self.Kd / self.Tm) * self.error + (-self.Kp + self.Ki * self.Tm - 2 * self.Kd / self.Tm) * self.error1 + (self.Kd / self.Tm) * self.error2
+        self.cv1 = self.cv
+        self.error2 = self.error1
+        self.error1 = self.error
+
+        if self.cv > 360:
+            self.cv = 360
+        if self.cv < 8:
+            self.cv = 8
+        
+        self.pwm.duty_u16(int(self.cv * (65535 / 360)))
+        
+    def off(self):
+        self.pwm.duty(0)
+
+    def get_cv(self):
+        return self.cv
