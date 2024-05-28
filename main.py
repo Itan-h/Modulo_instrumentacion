316037421b7c2ab908d76f391626d207ec845819
Eli_xz <elop679@gmail.com>
2024-05-28 09:35:49 -0600

    added drivers for tempeture sensors

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from machine import Pin, time
 import sensores as s
 from extras.rotary_irq import RotaryIRQ
-
+#caudal
 valve = s.Valvula(4)
 sensor = s.Caudalimetro(9)
 bomba = s.Bomba(15)
+#temperatura
+max1 = s.MAX6675(sck=Pin(1), so=Pin(1), cs=Pin(1))  # Sensor 1
+max2 = s.MAX6675(sck=Pin(1), so=Pin(1), cs=Pin(1))  # Sensor 2
+resistencia1 = s.resistencia(3)
+#nivel
 ultrasonic = s.Ultrasonico(12, 13)
 hzt1_tanque1 = s.Sensor_switch(6)
 hzt2_tanque1 = s.Sensor_switch(7)
 hzt1_tanque2 = s.Sensor_switch(8)
 hzt2_tanque2 = s.Sensor_switch(9)
+
 btn = Pin(18, Pin.IN, Pin.PULL_UP)#boton del encoder
 encoder = RotaryIRQ(pin_num_clk=21,
               pin_num_dt=19,
@@ -19,4 +25,5 @@ encoder = RotaryIRQ(pin_num_clk=21,
               range_mode=RotaryIRQ.RANGE_WRAP)
 
 while True:
+
     time.sleep(0.25)
